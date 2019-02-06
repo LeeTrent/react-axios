17c559457e5cfe33ecbbe9ff900dbe036ff888d9
LeeTrent <Lee.Trent.1@gmail.com>
2019-02-06 07:57:51 -0600

    Adds axios global interceptors for request and response to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log("[index.js][request]=>");
+    console.log(request);
+    console.log("<=[index.js][request]");
+    return request;
+}, error => {
+    console.log("[index.js][request][error]=>");
+    console.log(error);
+    console.log("<=[index.js][request][error]");  
+    return Promise.reject(error);  
+});
+
+axios.interceptors.response.use(response => {
+    console.log("[index.js][response]=>");
+    console.log(response);
+    console.log("<=[index.js][response]");
+    return response;
+}, error => {
+    console.log("[index.js][response][error]=>");
+    console.log(error);
+    console.log("<=[index.js][response][error]");  
+    return Promise.reject(error);  
+});
+
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
